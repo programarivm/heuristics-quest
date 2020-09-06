c3dd9a519c74a64d62c980160336b02d806e648d
programarivm <info@programarivm.com>
2020-09-06 13:33:47 +0100

    SevenTagRoster cleanup

--- src/components/SevenTagRoster.js
+++ src/components/SevenTagRoster.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
-const isHeuristic = (key) => {
-  return key === 'visualization' ||
-        key === 'center' ||
-        key === 'connectivity' ||
-        key === 'king_safety' ||
-        key === 'material' ||
-        key === 'space'
+const isBlacklist = (key) => {
+  return key === 'heuristic_picture'
 };
 
 export default function SevenTagRoster(game) {
   const items = [];
   Object.keys(game).forEach((key, index) => {
-    if (!isHeuristic(key)) {
+    if (!isBlacklist(key)) {
       if (key === 'movetext') {
         items.push(
           <Typography key={index} variant="subtitle2" color="textSecondary">
