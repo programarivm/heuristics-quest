713f88f1d22ce817e5b15cf1e345cfb130be6167
programarivm <info@programarivm.com>
2020-09-11 17:39:42 +0100

    overview information is displayed in a table

--- src/components/plot/Games.js
+++ src/components/plot/Games.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import clsx from 'clsx';
 import useStyles from 'components/plot/styles';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import SevenTagRoster from 'components/SevenTagRoster';
-import Typography from '@material-ui/core/Typography';
 import Chart from 'components/plot/Chart';
 import Subtotal from 'components/plot/Subtotal';
 import { prepareHeuristicPicture, calcSubtotal } from 'components/plot/utils';
@@ -14,11 +20,9 @@ export default function Games(params) {
   const apiQueryReducer = useSelector(state => state.apiQueryReducer);
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-
-  const overview = [];
   const items = [];
   const games = [];
-
+  const rows = [];
   if (apiQueryReducer.games[0]) {
     const dimension = JSON.parse(apiQueryReducer.games[0].heuristic_picture).w[0].length;
     const total = new Array(dimension).fill(null).map(() => ({ w: 0, b: 0 }));
@@ -42,9 +46,11 @@ export default function Games(params) {
       });
 
       items.push(
-        <Grid key={i} container spacing={3}>
+        <Grid key={i} container spacing={2}>
           <Grid item xs={12}>
-            <SevenTagRoster {...game} />
+            <Box mt={3}>
+              <SevenTagRoster {...game} />
+            </Box>
           </Grid>
           { charts }
         </Grid>
@@ -52,24 +58,38 @@ export default function Games(params) {
     });
 
     total.forEach((t, i) => {
-      overview.push(
-        <Grid key={i}>
-          <Typography variant="h4" gutterBottom>
-            <span style={{ color: "#82ca9d" }}>
-              {t.w.toFixed(2)}
-            </span>&nbsp;
-            <span style={{ color: "#ccc" }}>vs</span>&nbsp;
-            <span style={{ color: "#8884d8" }}>
-              {t.b.toFixed(2)}
-            </span>&nbsp;
-          </Typography>
-        </Grid>
-      );
+      rows.push({
+        name: i,
+        w: t.w.toLocaleString(),
+        b: t.b.toLocaleString(),
+        result: (t.w / t.b).toLocaleString()
+      });
     });
 
     games.push(
-      <Grid key={0} container spacing={3}>
-        { overview }
+      <Grid key={0} container spacing={2}>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Heuristic</TableCell>
+                <TableCell align="right">White</TableCell>
+                <TableCell align="right">Black</TableCell>
+                <TableCell align="right">White / Black</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">{row.name}</TableCell>
+                  <TableCell align="right">{row.w}</TableCell>
+                  <TableCell align="right">{row.b}</TableCell>
+                  <TableCell align="right">{row.result}</TableCell>
+                </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
         { items }
       </Grid>
     );
