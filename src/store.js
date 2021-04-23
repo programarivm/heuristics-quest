db45ef287b29b1cd834c01580362ff463a2e77f9
programarivm <info@programarivm.com>
2021-04-23 18:17:00 +0200

    Added Redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,5 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from './reducers/rootReducer';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk)),
+);
+
+export default store;
