7f917c20100ae1469fc321adfb716dbe4f42847b
programarivm <info@programarivm.com>
2021-04-25 14:47:19 +0200

    Cleaned up HeuristicPicture

--- src/components/plot/heuristic/Picture.js
+++ src/components/plot/heuristic/Picture.js
@@ -4,12 +4,11 @@ import Container from '@material-ui/core/Container';
 import Games from 'components/plot/Games';
 
 export default function HeuristicPicture() {
-  const name = 'heuristic-picture';
   const classes = useStyles();
 
   return (
     <Container maxWidth="lg" className={classes.container}>
-      <Games heuristic={name} />
+      <Games />
     </Container>
   );
 }
