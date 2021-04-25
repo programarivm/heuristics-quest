93b8758380e2a858720edf4f22ccf5f055d84c20
programarivm <info@programarivm.com>
2021-04-25 20:50:41 +0200

    Updated useStyles

--- src/App.js
+++ src/App.js
@@ -14,11 +14,6 @@ const useStyles = makeStyles((theme) => ({
   drawerPaper: {
     position: 'relative',
     whiteSpace: 'nowrap',
-    width: 60,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
   },
   content: {
     flexGrow: 1,
