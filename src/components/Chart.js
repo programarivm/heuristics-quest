4c5e287beba6ebb7f4ab98d5254c7b3b2d1d7625
programarivm <info@programarivm.com>
2021-04-29 17:11:06 +0200

    Cleaned up Chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CartesianGrid, Legend, LineChart, Line, XAxis, YAxis, ResponsiveContainer, Tooltip } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 
 export default function Chart(data) {
   return (
@@ -13,11 +13,8 @@ export default function Chart(data) {
             top: 5, right: 30, left: 20, bottom: 5,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="n" />
           <YAxis />
-          <Tooltip />
-          <Legend />
           <Line type="monotone" dataKey="w" stroke="#82ca9d" fill="#82ca9d" dot={false} strokeWidth={2} />
           <Line type="monotone" dataKey="b" stroke="#8884d8" fill="#8884d8" dot={false} strokeWidth={2} />
         </LineChart>
